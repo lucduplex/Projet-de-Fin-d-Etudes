74e9a0e3704f61fe682e85e5537f26e9ef6bac62
lucduplex <lucduplex@outlook.com>
2024-10-07 22:55:41 -0400

    update

--- store/store/store/settings.py
+++ store/store/store/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/store/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / '/static']
 
 
 
